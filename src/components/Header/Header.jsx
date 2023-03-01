39c7f136caa744c6f9ce39e8257321df79dfe1f8
dci-student <simaafshar1@gmail.com>
2023-03-01 14:19:45 +0100

    chamge the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
   return (
     <div className="d-flex flex-column flex-md-row align-items-center justify-content-between
 p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-  <h5 className="my-0 mr-md-auto font-weight-normal">Sign In App</h5>
+  <h5 className="my-0 mr-md-auto font-weight-normal">Sima's App</h5>
   <nav className="my-2 my-md-0 me-md-3">
     <Link className="p-2 text-dark" to={"/"}>Home</Link>
     {isAuthenticated ? <PrivateNavigation /> : <PublicNavigation />}
